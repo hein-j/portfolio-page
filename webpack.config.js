82a7a74292e185589ec8df37f48c8facda4af29d
Hein Jeong <hein.jeong@gmail.com>
2021-03-24 15:32:22 -0700

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+  // DEVELOPMENT
+  devtool: 'inline-source-map',
+  // DEVELOPMENT
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      cache: false,
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      // BABEL
+      // {
+      //   test: /\.m?js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: [
+      //         ['@babel/preset-env', { targets: 'defaults' }],
+      //       ],
+      //     },
+      //   },
+      // },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
