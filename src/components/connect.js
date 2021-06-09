c44f89e1c4ecf60af4803186b143794b5a3ed94a
Hein Jeong <hein.jeong@gmail.com>
2021-06-08 17:03:41 -0700

    Change email display mechanism to guard against crawlers

--- src/components/connect.js
+++ src/components/connect.js
@@ -1,7 +1,14 @@
 import '../styles/connect.css';
 
+const mailSpan = document.querySelector('.mail-container > span');
+
+const address = 'hein.jeong' + '@' + 'gmai' + 'l.com';
+
+const link = 'mai' + 'lto:' + address;
+
 const toggleEmail = () => {
-  document.querySelector('.mail-container > span').classList.toggle('expanded-email');
+  mailSpan.innerHTML = `<a href=${link}>${address}</a>`
+  mailSpan.classList.toggle('expanded-email');
 }
 
 document.querySelector('.mail-container > input').addEventListener('click', toggleEmail)
